8c3f0d799b9a63a39300d7a52317463b1423a643
hyunwoo <junghw86@naver.com>
2024-01-28 10:17:15 +0900

    feat: 상단 앱바 설정

---
+++ android/app/src/main/kotlin/com/example/fastcampus_part2_copy_instargram/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fastcampus_part2_copy_instargram
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const InstaCloneApp());
+}
+
+class InstaCloneApp extends StatelessWidget {
+  const InstaCloneApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: const ColorScheme.light(
+          primary: Colors.white,
+          secondary: Colors.black,
+        ),
+      ),
+      home: const InstaCloneHome(),
+    );
+  }
+}
+
+class InstaCloneHome extends StatelessWidget {
+  const InstaCloneHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Instagram',
+          style: GoogleFonts.lobsterTwo(color: Colors.black, fontSize: 32),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.favorite_outline,
+              size: 32,
+            ),
+            onPressed: () {
+              print('Tab favorite');
+            },
+          ),
+          IconButton(
+            icon: Icon(CupertinoIcons.paperplane, size: 32),
+            onPressed: () {
+              print('Tap papaerpalane');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
